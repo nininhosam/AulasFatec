ef71b0c768256a5d523c1473009d415f52120df9
Vinicius Forcato <76211125+nininhosam@users.noreply.github.com>
2023-05-02 10:43:42 -0300

    Created and finished "Desafio 3"

---
+++ Programação em Microinformática/Desafio 3/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+
+app = Flask("__name__")
+
+# configs
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'fatec'
+app.config['MYSQL_DB'] = 'desafio3'
+
+mysql = MySQL(app)
+
+@app.route("/")
+def home():
+  return render_template("home.html")
+
+@app.route("/quemsomos")
+def quemSomos():
+  return render_template("quemsomos.html")
+
+@app.route("/contato", methods=['GET', 'POST'])
+def contato():
+  if request.method == "POST":
+    email = request.form['email']
+    assunto = request.form['assunto']
+    descricao = request.form['descricao']
+
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO contatos (email, assunto, descricao) VALUES (%s, %s, %s)", (email, assunto, descricao))
+    mysql.connection.commit()
+    cur.close()
+
+    return 'success'
+  return render_template("contato.html")
